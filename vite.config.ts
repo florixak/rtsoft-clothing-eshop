6e2f0cddaa4c0dc65c0c06aa7f111e20d8841bde
ptakondrej <florixakcz@seznam.cz>
2026-03-07 21:33:12 +0100

    chore: setup shadcn/ui

--- vite.config.ts
+++ vite.config.ts
@@ -2,9 +2,15 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import { tanstackRouter } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
+import path from "node:path";
 
 // https://vite.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
   plugins: [
     tailwindcss(),
     tanstackRouter({ target: "react", autoCodeSplitting: true }),
