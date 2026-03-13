573442350757e400cddad433283154ed0d343b3b
ptakondrej <florixakcz@seznam.cz>
2026-03-13 18:43:43 +0100

    chore: export all mock data from index file

---
+++ src/data/index.ts
@@ -0,0 +1,6 @@
+export { categories } from "./categories";
+export { products } from "./products";
+export { shippingMethods, paymentMethods } from "./shipping";
+export { mockCart } from "./cart";
+export { orders } from "./orders";
+export { dashboardStats } from "./stats";
