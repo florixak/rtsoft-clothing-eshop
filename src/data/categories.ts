61dfdfa5bb66d4c4b0d3cc665c32234682335648
ptakondrej <florixakcz@seznam.cz>
2026-03-13 18:39:06 +0100

    chore: create category mock data

---
+++ src/data/categories.ts
@@ -0,0 +1,24 @@
+import type { Category } from "../types";
+
+export const categories: Category[] = [
+  {
+    id: "cat-1",
+    slug: { cs: "tricka", en: "t-shirts" },
+    name: { cs: "Trička", en: "T-Shirts" },
+  },
+  {
+    id: "cat-2",
+    slug: { cs: "mikiny", en: "hoodies" },
+    name: { cs: "Mikiny", en: "Hoodies" },
+  },
+  {
+    id: "cat-3",
+    slug: { cs: "kalhoty", en: "pants" },
+    name: { cs: "Kalhoty", en: "Pants" },
+  },
+  {
+    id: "cat-4",
+    slug: { cs: "bundy", en: "jackets" },
+    name: { cs: "Bundy", en: "Jackets" },
+  },
+];
