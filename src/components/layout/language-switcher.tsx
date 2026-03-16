017dfb1e2a2c4345db95a5d5cebd943ef909058d
ptakondrej <florixakcz@seznam.cz>
2026-03-16 16:53:46 +0100

    feat: create language switchers

---
+++ src/components/layout/language-switcher.tsx
@@ -0,0 +1,61 @@
+import useLanguage from "@/hooks/use-language";
+import { useTranslation } from "react-i18next";
+import { Button } from "../ui/button";
+import { ButtonGroup } from "../ui/button-group";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "../ui/select";
+import { Languages } from "lucide-react";
+
+const LanguageSwitcher = () => {
+  const { t } = useTranslation("common");
+  const { currentLanguage, changeLanguage } = useLanguage();
+
+  return (
+    <Select value={currentLanguage} onValueChange={changeLanguage}>
+      <SelectTrigger>
+        <SelectValue>{t(`language.${currentLanguage}`)}</SelectValue>
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="cs">{t("language.cs")}</SelectItem>
+        <SelectItem value="en">{t("language.en")}</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+const MobileLanguageSwitcher = () => {
+  const { t } = useTranslation("common");
+  const { changeLanguage, currentLanguage } = useLanguage();
+
+  return (
+    <div className="flex flex-row items-center justify-between">
+      <div className="relative flex items-center gap-2 text-sm font-medium">
+        <Languages size={24} className="h-[1.2rem] w-[1.2rem]" />
+        {t("header.menu.language")}
+      </div>
+      <ButtonGroup>
+        <Button
+          variant={currentLanguage === "cs" ? "default" : "outline"}
+          onClick={() => changeLanguage("cs")}
+          aria-label={t("language.cs")}
+        >
+          {t("language.cs")}
+        </Button>
+        <Button
+          variant={currentLanguage === "en" ? "default" : "outline"}
+          onClick={() => changeLanguage("en")}
+          aria-label={t("language.en")}
+        >
+          {t("language.en")}
+        </Button>
+      </ButtonGroup>
+    </div>
+  );
+};
+
+export { LanguageSwitcher, MobileLanguageSwitcher };
