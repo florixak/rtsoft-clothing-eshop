65a6ff26ca1ecd3655ad31845fcb7f8d71c39471
ptakondrej <florixakcz@seznam.cz>
2026-03-16 16:53:21 +0100

    feat: create custom hook for language change

---
+++ src/hooks/use-language.ts
@@ -0,0 +1,32 @@
+import { useLocation, useNavigate } from "@tanstack/react-router";
+import { useTranslation } from "react-i18next";
+
+const useLanguage = () => {
+  const { i18n } = useTranslation();
+  const navigate = useNavigate();
+  const location = useLocation();
+  const currentLanguage = i18n.resolvedLanguage === "en" ? "en" : "cs";
+
+  const changeLanguage = async (value: string | null) => {
+    if (value === "cs" || value === "en") {
+      if (value === currentLanguage) return;
+
+      const pathnameWithoutLocale = location.pathname.replace(
+        /^\/(cs|en)(?=\/|$)/,
+        "",
+      );
+      const localizedPath = `/${value}${pathnameWithoutLocale || "/"}`;
+
+      await i18n.changeLanguage(value);
+      await navigate({
+        to: localizedPath,
+        replace: true,
+        search: location.search,
+        hash: location.hash,
+      });
+    }
+  };
+  return { currentLanguage, changeLanguage };
+};
+
+export default useLanguage;
