c1401a9cc8021076be35ed65e3965a31f4ffac68
ptakondrej <florixakcz@seznam.cz>
2026-03-17 19:57:53 +0100

    feat: uncomment header search component

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -6,6 +6,7 @@ import MobileMenuDrawer from "./mobile-menu-drawer";
 import { ThemeSwitcher } from "./theme-switcher";
 import { LanguageSwitcher } from "./language-switcher";
 import { useCartStore } from "@/stores/cart-store";
+import HeaderSearch from "./header-search";
 
 const Header = () => {
   const { t } = useTranslation("common");
@@ -22,7 +23,7 @@ const Header = () => {
           <ShoppingBag size={32} className="inline-block mr-2" />
           <h1 className="text-3xl font-heading">E-Shop</h1>
         </Link>
-        {/*<HeaderSearch mode="desktop" />*/}
+        <HeaderSearch mode="desktop" />
         <nav className="flex flex-1 justify-end">
           <ul className="flex flex-row items-center gap-4">
             <li className="hidden md:block">
@@ -34,7 +35,7 @@ const Header = () => {
               <Link to="/{-$locale}/cart" aria-label={t("header.aria.cart")}>
                 <ShoppingCart size={24} />
               </Link>
-              <span className="absolute -top-1 -right-1 text-xs bg-primary text-white rounded-full px-1">
+              <span className="absolute -top-1 -right-1 text-xs bg-primary text-primary-foreground rounded-full px-1">
                 {itemsCount()}
               </span>
             </li>
@@ -59,7 +60,9 @@ const Header = () => {
         </nav>
       </div>
 
-      <div className="mt-3 md:hidden">{/*<HeaderSearch mode="mobile" />*/}</div>
+      <div className="mt-3 md:hidden">
+        <HeaderSearch mode="mobile" />
+      </div>
     </header>
   );
 };
