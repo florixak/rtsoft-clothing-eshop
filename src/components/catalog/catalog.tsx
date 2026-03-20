41925b0457348b0cdd994e18b829ed27191515f2
ptakondrej <florixakcz@seznam.cz>
2026-03-20 06:20:04 +0100

    feat: add filter and enchance product card

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -1,6 +1,9 @@
 import { products } from "@/data";
 import { formatPrice } from "@/lib/utils";
+import { useCartStore } from "@/stores/cart-store";
+import type { SizeCode, TypeCode } from "@/types";
 import { ListFilter, ShoppingBasket, Star, X } from "lucide-react";
+import { useTranslation } from "react-i18next";
 import { Badge } from "../ui/badge";
 import { Button } from "../ui/button";
 import { Card, CardContent, CardHeader } from "../ui/card";
@@ -12,18 +15,34 @@ import {
   DrawerTitle,
   DrawerTrigger,
 } from "../ui/drawer";
-import { useTranslation } from "react-i18next";
+import { Link } from "@tanstack/react-router";
+import CatalogFilter from "./catalog-filter";
 
 const Catalog = () => {
+  const { addItem } = useCartStore();
   const { t, i18n } = useTranslation("catalog");
-  const countOfProducts = 48;
+  const countOfProducts = products.length;
   const showingFrom = 1;
-  const showingTo = 12;
+  const showingTo = countOfProducts < 12 ? countOfProducts : 12;
 
   const locale = i18n.resolvedLanguage === "en" ? "en" : "cs";
 
+  const handleAddToCart = (
+    productId: string,
+    defaultSize: SizeCode,
+    defaultColor?: TypeCode,
+    defaultMaterial?: TypeCode,
+  ) => {
+    addItem({
+      productId,
+      size: defaultSize,
+      color: defaultColor,
+      material: defaultMaterial,
+    });
+  };
+
   return (
-    <section>
+    <section className="container mx-auto">
       <div className="flex flex-col gap-2">
         <div className="flex flex-row items-center justify-between">
           <h2 className="text-2xl font-heading font-semibold">
@@ -61,29 +80,56 @@ const Catalog = () => {
                     Refine your product search
                   </DrawerDescription>
                 </DrawerHeader>
-                <DrawerContent>{/* Filter options go here */}</DrawerContent>
+                <DrawerContent>
+                  <CatalogFilter />
+                </DrawerContent>
               </DrawerContent>
             </Drawer>
           </div>
         </div>
       </div>
-      <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+      <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
         {products.map((product) => (
-          <Card className="p-0 shadow-sm gap-2" key={product.id}>
+          <Card
+            className="p-0 shadow-sm gap-2 overflow-hidden group"
+            key={product.id}
+          >
             <CardHeader className="p-0">
               <img
                 src={product.images[0]}
                 alt={product.name[locale]}
-                className="size-100 w-full object-cover"
+                className="max-h-100 md:max-h-80 w-full object-cover group-hover:hidden"
+                loading="lazy"
+              />
+              <img
+                src={product.images[1] || product.images[0]}
+                alt={product.name[locale]}
+                className="max-h-100 md:max-h-80 w-full object-cover hidden group-hover:block"
+                loading="lazy"
               />
             </CardHeader>
-            <CardContent className="pb-4 flex flex-col gap-2">
+            <CardContent className="pb-4 flex flex-col gap-4 justify-between h-full">
               <div className="flex flex-col gap-1">
-                <div key={product.id} className="flex items-center gap-1">
+                <div
+                  key={product.id}
+                  className="flex flex-row items-center gap-1"
+                >
                   <Star size={16} className="inline-block text-yellow-500" />
                   {product.rating}
+                  <span className="text-xs text-muted-foreground">
+                    {t("productCard.reviews", {
+                      count: product.reviewsCount,
+                    })}
+                  </span>
                 </div>
-                <h3 className="text-lg font-bold">{product.name[locale]}</h3>
+                <h3 className="text-lg font-bold hover:underline cursor-pointer">
+                  <Link
+                    to="/{-$locale}/product/$productSlug"
+                    params={{ locale, productSlug: product.slug[locale] }}
+                  >
+                    {product.name[locale]}
+                  </Link>
+                </h3>
                 <p className="text-muted-foreground">
                   {product.description[locale]}
                 </p>
@@ -92,7 +138,19 @@ const Catalog = () => {
                 <p className="text-lg font-bold">
                   {formatPrice(product.price, locale)}
                 </p>
-                <Button variant="outline" size="default">
+                <Button
+                  variant="default"
+                  size="default"
+                  className="cursor-pointer"
+                  onClick={() =>
+                    handleAddToCart(
+                      product.id,
+                      product.options.sizes[0].code,
+                      product.options.colors?.[0]?.code,
+                      product.options.material?.[0]?.code,
+                    )
+                  }
+                >
                   <ShoppingBasket size={16} />
                   {t("productCard.addToCart")}
                 </Button>
