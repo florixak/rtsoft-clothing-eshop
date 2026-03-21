7937bb9f72d985e96e1f2e1c4e18e76019c29339
ptakondrej <florixakcz@seznam.cz>
2026-03-21 07:03:37 +0100

    feat: fetch data and add navigation component

--- src/components/catalog/catalog.tsx
+++ src/components/catalog/catalog.tsx
@@ -1,16 +1,61 @@
-import { products } from "@/data";
+import { getProducts } from "@/lib/product-utils";
+import useDebounce from "@/hooks/use-debounce";
+import { useQuery } from "@tanstack/react-query";
+import { useNavigate, useSearch } from "@tanstack/react-router";
+import Navigation from "../layout/navigation";
 import CatalogHeader from "./catalog-header";
 import ProductCard from "./product-card";
 
 const Catalog = () => {
+  const search = useSearch({ from: "/{-$locale}/" });
+  const { debouncedValue: debouncedSearch } = useDebounce({
+    value: search,
+    delay: 750,
+  });
+  const navigate = useNavigate({ from: "/{-$locale}/" });
+  const page = search.page ?? 1;
+  const perPage = search.perPage ?? 12;
+
+  const {
+    data: { products, information } = {
+      products: [],
+      information: { total: 0, maxFilterPrice: 0, minFilterPrice: 0 },
+    },
+  } = useQuery({
+    queryKey: ["products", debouncedSearch],
+    queryFn: () => getProducts(debouncedSearch),
+  });
+
+  const totalPages = Math.max(1, Math.ceil((information.total ?? 0) / perPage));
+  const safePage = Math.min(Math.max(page, 1), totalPages);
+
   return (
     <section className="container mx-auto">
       <CatalogHeader />
-      <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-6">
+      <div className="mt-6 grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6">
         {products.map((product) => (
           <ProductCard key={product.id} product={product} />
         ))}
       </div>
+      <Navigation
+        page={safePage}
+        pageSize={perPage}
+        pagedItems={products}
+        totalItemsCount={information.total ?? 0}
+        showSummary={true}
+        onPageChange={(nextPage) => {
+          navigate({
+            search: (prev) => ({ ...prev, page: nextPage }),
+            replace: true,
+          });
+        }}
+        onPageSizeChange={(nextPageSize) => {
+          navigate({
+            search: (prev) => ({ ...prev, perPage: nextPageSize, page: 1 }),
+            replace: true,
+          });
+        }}
+      />
     </section>
   );
 };
