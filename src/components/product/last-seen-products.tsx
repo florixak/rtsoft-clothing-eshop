9a2512ba39c86c825539f8631b553e2ab4621c8b
ptakondrej <florixakcz@seznam.cz>
2026-03-24 14:06:06 +0100

    feat: display last seen products

--- src/components/product/last-seen-products.tsx
+++ src/components/product/last-seen-products.tsx
@@ -1,24 +1,32 @@
 import { TRANSLATION_NAMESPACES } from "@/lib/i18n";
+import { getLastSeenProducts } from "@/lib/product-utils";
 import type { Product } from "@/types";
 import { useTranslation } from "react-i18next";
+import ProductCard from "../catalog/product-card";
 
 type LastSeenProductsProps = {
   product: Product;
 };
 
 const LastSeenProducts = ({ product }: LastSeenProductsProps) => {
-  const { t, i18n } = useTranslation(TRANSLATION_NAMESPACES.product);
-  const locale = i18n.resolvedLanguage === "en" ? "en" : "cs";
+  const { t } = useTranslation(TRANSLATION_NAMESPACES.product);
+  const lastSeenProducts = getLastSeenProducts(3, product.id);
   return (
-    <div className="flex flex-col gap-2">
-      <div className="flex flex-col gap-2">
+    <div className="flex flex-col gap-4">
+      <div className="flex flex-col">
         <h2 className="text-xl font-bold uppercase">{t("lastSeen.title")}</h2>
         <p className="text-muted-foreground">
           {t("lastSeen.subtitle", { count: product?.reviewsCount })}
         </p>
       </div>
 
-      <div></div>
+      <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+        {lastSeenProducts.length > 0 ? (
+          lastSeenProducts.map((p) => <ProductCard key={p.id} product={p} />)
+        ) : (
+          <p className="text-muted-foreground">{t("lastSeen.noProducts")}</p>
+        )}
+      </div>
     </div>
   );
 };
