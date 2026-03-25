4c60c64c1b16aaa2945585a60c524d88a88ac467
ptakondrej <florixakcz@seznam.cz>
2026-03-25 16:21:14 +0100

    feat: create cart item

---
+++ src/components/cart/cart-item.tsx
@@ -0,0 +1,76 @@
+import { createProductIdQueryOptions } from "@/hooks/query-options";
+import { TRANSLATION_NAMESPACES } from "@/lib/i18n";
+import { formatPrice } from "@/lib/utils";
+import { useCartStore } from "@/stores/cart-store";
+import type { CartItem as CartItemType } from "@/types";
+import { useQuery } from "@tanstack/react-query";
+import { Trash } from "lucide-react";
+import { useTranslation } from "react-i18next";
+import QuantityCounter from "../product/quantity-counter";
+import { Button } from "../ui/button";
+
+type CartItemProps = {
+  item: CartItemType;
+};
+
+const CartItem = ({ item }: CartItemProps) => {
+  const { removeItem, changeItemQuantity } = useCartStore();
+  const { data: product } = useQuery(
+    createProductIdQueryOptions(item.productId),
+  );
+  const { t, i18n } = useTranslation(TRANSLATION_NAMESPACES.cart);
+  const locale = i18n.resolvedLanguage === "en" ? "en" : "cs";
+
+  const handleRemove = () => {
+    removeItem(item.id);
+  };
+
+  const handleQuantityChange = (newQuantity: number) => {
+    changeItemQuantity(item.id, newQuantity);
+  };
+
+  if (!product) {
+    return (
+      <div>
+        {t("item.size", { size: item.selectionSnapshot.size })} |{" "}
+        {t("item.color", { color: item.selectionSnapshot.color })} - $
+        {formatPrice(item.priceSnapshot * item.quantity, locale)}
+      </div>
+    );
+  }
+
+  return (
+    <div className="flex flex-col md:flex-row items-start w-full gap-4">
+      <img
+        src={product?.images[0] ?? ""}
+        alt={product?.name[locale] ?? ""}
+        className="size-64 object-cover rounded"
+      />
+      <div className="flex flex-col justify-between flex-1 gap-2 w-full h-full">
+        <div className="flex flex-col gap-1">
+          <h2 className="text-xl font-bold">{product?.name[locale] ?? ""}</h2>
+          <p className="text-sm text-muted-foreground">
+            {t("item.size", {
+              size: item.selectionSnapshot.size.toUpperCase(),
+            })}{" "}
+            | {t("item.color", { color: item.selectionSnapshot.color })}
+          </p>
+          <p className="text-lg font-bold">
+            {formatPrice(item.priceSnapshot * item.quantity, locale)}
+          </p>
+        </div>
+        <div className="flex items-center gap-4">
+          <QuantityCounter
+            quantity={item.quantity}
+            onQuantityChange={handleQuantityChange}
+          />
+          <Button variant="outline" onClick={handleRemove} className="h-8">
+            <Trash size={16} />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default CartItem;
