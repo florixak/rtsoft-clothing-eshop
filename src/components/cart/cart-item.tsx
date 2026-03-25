00531838f924d5d7f3869139445da065ba143592
ptakondrej <florixakcz@seznam.cz>
2026-03-25 18:28:11 +0100

    feat: add compact version of cart item

--- src/components/cart/cart-item.tsx
+++ src/components/cart/cart-item.tsx
@@ -11,9 +11,10 @@ import { Button } from "../ui/button";
 
 type CartItemProps = {
   item: CartItemType;
+  compact?: boolean;
 };
 
-const CartItem = ({ item }: CartItemProps) => {
+const CartItem = ({ item, compact = false }: CartItemProps) => {
   const { removeItem, changeItemQuantity } = useCartStore();
   const { data: product } = useQuery(
     createProductIdQueryOptions(item.productId),
@@ -39,6 +40,31 @@ const CartItem = ({ item }: CartItemProps) => {
     );
   }
 
+  const price = formatPrice(item.priceSnapshot * item.quantity, locale);
+
+  if (compact) {
+    return (
+      <div className="flex items-center gap-4">
+        <img
+          src={product.images[0]}
+          alt={product.name[locale]}
+          className="size-16 object-cover rounded"
+        />
+
+        <div className="flex-1">
+          <h2 className="text-sm font-medium">{product.name[locale]}</h2>
+          <p className="text-sm text-muted-foreground">
+            {t("item.size", {
+              size: item.selectionSnapshot.size.toUpperCase(),
+            })}{" "}
+            | {t("item.color", { color: item.selectionSnapshot.color })}
+          </p>
+          <p className="text-sm font-semibold text-muted-foreground">{price}</p>
+        </div>
+      </div>
+    );
+  }
+
   return (
     <div className="flex flex-col md:flex-row items-start w-full gap-4">
       <img
@@ -55,9 +81,7 @@ const CartItem = ({ item }: CartItemProps) => {
             })}{" "}
             | {t("item.color", { color: item.selectionSnapshot.color })}
           </p>
-          <p className="text-lg font-bold">
-            {formatPrice(item.priceSnapshot * item.quantity, locale)}
-          </p>
+          <p className="text-lg font-bold">{price}</p>
         </div>
         <div className="flex items-center gap-4">
           <QuantityCounter
