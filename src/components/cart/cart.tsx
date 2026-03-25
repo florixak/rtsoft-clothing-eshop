b8735da21f27da548372fbb8dfbdd13eb1906264
ptakondrej <florixakcz@seznam.cz>
2026-03-25 21:23:19 +0100

    feat: shrink cart container

--- src/components/cart/cart.tsx
+++ src/components/cart/cart.tsx
@@ -8,7 +8,7 @@ const Cart = () => {
   const { itemsCount } = useCartStore();
   const { t } = useTranslation(TRANSLATION_NAMESPACES.cart);
   return (
-    <section className="max-w-7xl mx-auto space-y-8">
+    <section className="max-w-6xl mx-auto space-y-8">
       <div className="flex flex-col gap-2">
         <h1 className="text-2xl font-heading font-bold">{t("title")}</h1>
         <p className="text-muted-foreground">
