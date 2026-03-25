30e39c01a4aaca8677bd3e84733db2dc09b1ebb0
ptakondrej <florixakcz@seznam.cz>
2026-03-25 21:25:10 +0100

    feat: option to show/hide more than x items

--- src/components/cart/cart-items.tsx
+++ src/components/cart/cart-items.tsx
@@ -4,13 +4,22 @@ import { TRANSLATION_NAMESPACES } from "@/lib/i18n";
 import { useTranslation } from "react-i18next";
 import { Button } from "../ui/button";
 import { Link } from "@tanstack/react-router";
+import { useState } from "react";
+import { CART_ITEM_SHOW_MORE } from "@/constants";
 
 const CartItems = () => {
   const {
     cart: { items },
   } = useCartStore();
+  const [showAll, setShowAll] = useState(false);
 
   const { t } = useTranslation(TRANSLATION_NAMESPACES.cart);
+
+  const visibleItems = showAll ? items : items.slice(0, CART_ITEM_SHOW_MORE);
+
+  const handleShowMore = () => {
+    setShowAll((prev) => !prev);
+  };
 
   if (items.length === 0) {
     return (
@@ -26,9 +35,14 @@ const CartItems = () => {
 
   return (
     <div className="flex flex-col gap-8 w-full">
-      {items.map((item) => (
+      {visibleItems.map((item) => (
         <CartItem key={item.id} item={item} />
       ))}
+      {items.length > CART_ITEM_SHOW_MORE && (
+        <Button variant="outline" onClick={handleShowMore}>
+          {showAll ? t("actions.showLess") : t("actions.showMore")}
+        </Button>
+      )}
     </div>
   );
 };
