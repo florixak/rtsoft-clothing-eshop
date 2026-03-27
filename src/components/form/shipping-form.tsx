a187c455c155cc0261bd7e43d6764abe03b1eccb
ptakondrej <florixakcz@seznam.cz>
2026-03-27 21:41:43 +0100

    feat: finish shipping form

--- src/components/form/shipping-form.tsx
+++ src/components/form/shipping-form.tsx
@@ -11,28 +11,118 @@ const ShippingForm = withForm({
     const locale = i18n.resolvedLanguage == "cs" ? "cs" : "en";
     const translation = i18n.getFixedT(locale, TRANSLATION_NAMESPACES.checkout);
     return (
-      <div className="flex flex-col gap-4 w-full">
-        <div className="flex flex-col md:flex-row w-full gap-2">
-          {shippingMethods.map((method) => (
-            <form.AppField
-              key={method.id}
-              name="shipping.shippingMethod"
-              children={(field) => (
-                <field.RadioButtonField value={method.id} className="w-full">
-                  <CardHeader>{method.name[locale]}</CardHeader>
-                  <CardContent className="text-sm text-muted-foreground flex flex-col gap-2 w-full">
-                    <p>{method.description[locale]}</p>
+      <div className="flex flex-col gap-8 w-full">
+        <div className="flex flex-col gap-6">
+          <h3 className="text-lg font-semibold">
+            <span className="bg-primary text-primary-foreground rounded-full px-3 py-1 mr-2">
+              1
+            </span>
+            {translation("shippingMethod.title")}
+          </h3>
+          <div className="flex flex-col md:flex-row w-full gap-2">
+            {shippingMethods.map((method) => (
+              <form.AppField
+                key={method.id}
+                name="shipping.shippingMethod"
+                children={(field) => (
+                  <field.RadioButtonField value={method.id} className="w-full">
+                    <CardHeader>{method.name[locale]}</CardHeader>
+                    <CardContent className="text-sm text-muted-foreground flex flex-col gap-2 w-full">
+                      <p>{method.description[locale]}</p>
 
-                    <span className="font-semibold text-lg">
-                      {method.price === 0
-                        ? translation("shippingMethod.free")
-                        : formatPrice(method.price, locale)}
-                    </span>
-                  </CardContent>
-                </field.RadioButtonField>
+                      <span className="font-semibold text-lg">
+                        {method.price === 0
+                          ? translation("shippingMethod.free")
+                          : formatPrice(method.price, locale)}
+                      </span>
+                    </CardContent>
+                  </field.RadioButtonField>
+                )}
+              />
+            ))}
+          </div>
+        </div>
+        <div className="flex flex-col gap-6">
+          <h3 className="text-lg font-semibold">
+            <span className="bg-primary text-primary-foreground rounded-full px-3 py-1 mr-2">
+              2
+            </span>
+            {translation("deliveryInfo.title")}
+          </h3>
+          <div>{translation("deliveryInfo.description")}</div>
+          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 w-full">
+            <form.AppField
+              name="shipping.firstName"
+              children={(field) => (
+                <field.TextField
+                  label={translation("deliveryInfo.fields.firstName.label")}
+                  placeholder={translation(
+                    "deliveryInfo.fields.firstName.placeholder",
+                  )}
+                />
               )}
             />
-          ))}
+            <form.AppField
+              name="shipping.lastName"
+              children={(field) => (
+                <field.TextField
+                  label={translation("deliveryInfo.fields.lastName.label")}
+                  placeholder={translation(
+                    "deliveryInfo.fields.lastName.placeholder",
+                  )}
+                />
+              )}
+            />
+            <div className="md:col-span-2">
+              <form.AppField
+                name="shipping.streetAddress"
+                children={(field) => (
+                  <field.TextField
+                    label={translation("deliveryInfo.fields.street.label")}
+                    placeholder={translation(
+                      "deliveryInfo.fields.street.placeholder",
+                    )}
+                  />
+                )}
+              />
+            </div>
+
+            <form.AppField
+              name="shipping.city"
+              children={(field) => (
+                <field.TextField
+                  label={translation("deliveryInfo.fields.city.label")}
+                  placeholder={translation(
+                    "deliveryInfo.fields.city.placeholder",
+                  )}
+                />
+              )}
+            />
+            <form.AppField
+              name="shipping.postalCode"
+              children={(field) => (
+                <field.TextField
+                  label={translation("deliveryInfo.fields.postalCode.label")}
+                  placeholder={translation(
+                    "deliveryInfo.fields.postalCode.placeholder",
+                  )}
+                />
+              )}
+            />
+            <div className="md:col-span-2">
+              <form.AppField
+                name="shipping.country"
+                children={(field) => (
+                  <field.TextField
+                    label={translation("deliveryInfo.fields.country.label")}
+                    placeholder={translation(
+                      "deliveryInfo.fields.country.placeholder",
+                    )}
+                  />
+                )}
+              />
+            </div>
+          </div>
         </div>
       </div>
     );
