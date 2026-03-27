c1fe461003b60e6403a4c821202f916f8ce2d1e5
ptakondrej <florixakcz@seznam.cz>
2026-03-27 22:21:58 +0100

    feat: add active indicator

--- src/components/form/radio-button-field.tsx
+++ src/components/form/radio-button-field.tsx
@@ -1,6 +1,7 @@
 import { useFieldContext } from "@/hooks/form-context";
 import { Card } from "../ui/card";
 import clsx from "clsx";
+import { Check } from "lucide-react";
 
 type RadioButtonFieldProps = {
   children: React.ReactNode;
@@ -17,12 +18,20 @@ const RadioButtonField = ({
   return (
     <Card
       className={clsx(
-        `${field.state.value === value ? "border-primary" : "border-muted"} cursor-pointer border-2`,
+        `${field.state.value === value ? "border-primary" : "border-muted"} cursor-pointer border-2 relative`,
         className,
       )}
       onClick={() => field.handleChange(value)}
       onBlur={field.handleBlur}
     >
+      <span
+        className={clsx(
+          "absolute top-2 right-2 bg-primary text-primary-foreground rounded-full p-1",
+          field.state.value === value ? "opacity-100" : "opacity-0",
+        )}
+      >
+        <Check size={16} />
+      </span>
       {children}
     </Card>
   );
