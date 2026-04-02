a6775a7a714413f2b89132689ced7f53828825e0
ptakondrej <florixakcz@seznam.cz>
2026-04-02 18:45:03 +0200

    feat: prefetching order for order confirmation

--- src/routes/{-$locale}/checkout/success.tsx
+++ src/routes/{-$locale}/checkout/success.tsx
@@ -1,7 +1,27 @@
+import { createCheckoutOrderQueryOptions } from "@/hooks/query-options";
 import { createFileRoute } from "@tanstack/react-router";
+import * as z from "zod";
+
+const successSchema = z.object({
+  orderId: z.string(),
+});
 
 export const Route = createFileRoute("/{-$locale}/checkout/success")({
   component: RouteComponent,
+  validateSearch: successSchema,
+  loaderDeps: ({ search }) => [search.orderId],
+  loader: async ({ context, deps }) => {
+    const orderId = deps[0];
+    const order = await context.queryClient.ensureQueryData(
+      createCheckoutOrderQueryOptions(orderId),
+    );
+
+    console.log("Loaded order in success page:", order);
+
+    if (!order) {
+      throw new Response("Order not found", { status: 404 });
+    }
+  },
 });
 
 function RouteComponent() {
