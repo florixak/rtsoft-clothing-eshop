fc86dd98c8a683a052e461d34e6152f1011d5bc2
ptakondrej <florixakcz@seznam.cz>
2026-04-03 11:17:51 +0200

    feat: show success order component

--- src/routes/{-$locale}/checkout/success.tsx
+++ src/routes/{-$locale}/checkout/success.tsx
@@ -1,3 +1,4 @@
+import CheckoutSuccess from "@/components/checkout/checkout-success";
 import { createCheckoutOrderQueryOptions } from "@/hooks/query-options";
 import { createFileRoute } from "@tanstack/react-router";
 import * as z from "zod";
@@ -10,13 +11,13 @@ export const Route = createFileRoute("/{-$locale}/checkout/success")({
   component: RouteComponent,
   validateSearch: successSchema,
   loaderDeps: ({ search }) => [search.orderId],
+  errorComponent: () => <div>Order not found</div>,
+  pendingComponent: () => <div>Loading...</div>,
   loader: async ({ context, deps }) => {
     const orderId = deps[0];
     const order = await context.queryClient.ensureQueryData(
       createCheckoutOrderQueryOptions(orderId),
     );
-
-    console.log("Loaded order in success page:", order);
 
     if (!order) {
       throw new Response("Order not found", { status: 404 });
@@ -25,5 +26,5 @@ export const Route = createFileRoute("/{-$locale}/checkout/success")({
 });
 
 function RouteComponent() {
-  return <div>Hello "/-$locale/checkout/success"!</div>;
+  return <CheckoutSuccess />;
 }
