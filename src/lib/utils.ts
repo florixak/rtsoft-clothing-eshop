9a9a182c661606b4aa830e7b0b6d0b5308096951
ptakondrej <florixakcz@seznam.cz>
2026-04-06 11:16:35 +0200

    feat: add format date util

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,15 +1,23 @@
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import type { Languages } from "./i18n";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export const formatPrice = (price: number, locale: string) => {
+export const formatPrice = (price: number, locale: Languages) => {
   return new Intl.NumberFormat(locale, {
     style: "currency",
     currency: "CZK",
   }).format(price);
+};
+
+export const formatDate = (date: string, locale: Languages) => {
+  return new Intl.DateTimeFormat(locale, {
+    dateStyle: "medium",
+    timeStyle: "short",
+  }).format(new Date(date));
 };
 
 export const clamp = (value: number, min: number, max: number) => {
