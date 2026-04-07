1fafade1b409cf4460a4d5eb2164cac1d910774b
ptakondrej <florixakcz@seznam.cz>
2026-04-07 09:55:53 +0200

    feat: display admin orders table

---
+++ src/components/admin/orders/admin-orders.tsx
@@ -0,0 +1,27 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { Skeleton } from "@/components/ui/skeleton";
+import { TRANSLATION_NAMESPACES } from "@/lib/i18n";
+import { useTranslation } from "react-i18next";
+import AdminOrdersTable from "./admin-orders-table";
+import { Suspense } from "react";
+
+const AdminOrders = () => {
+  const { t } = useTranslation(TRANSLATION_NAMESPACES.admin);
+  return (
+    <Card>
+      <CardHeader>
+        <h2 className="text-lg font-semibold">
+          {t("overview.recentTransactions.title")}
+        </h2>
+      </CardHeader>
+
+      <CardContent>
+        <Suspense fallback={<Skeleton className="h-48 w-full" />}>
+          <AdminOrdersTable />
+        </Suspense>
+      </CardContent>
+    </Card>
+  );
+};
+
+export default AdminOrders;
