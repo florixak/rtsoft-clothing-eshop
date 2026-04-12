9842fb158d84964e1c2229a19c326de54777dec0
ptakondrej <florixakcz@seznam.cz>
2026-04-12 13:50:40 +0200

    feat: create admin product columns

---
+++ src/components/table/product-columns.tsx
@@ -0,0 +1,52 @@
+import type { Product } from "@/types";
+import { createColumnHelper } from "@tanstack/react-table";
+import type { TFunction } from "i18next";
+import createSortableHeader from "./sortable-header";
+import { formatDate, formatPrice } from "@/lib/utils";
+import type { Languages } from "@/lib/i18n";
+
+type ProductColumnsParams = {
+  locale: Languages;
+  t: TFunction<"admin", undefined>;
+};
+
+const columnHelper = createColumnHelper<Product>();
+
+export const createAdminProductColumns = ({
+  locale,
+  t,
+}: ProductColumnsParams) => {
+  return [
+    columnHelper.accessor("id", {
+      header: () => t("products.table.productId"),
+      cell: ({ row }) => (
+        <span className="font-medium">{row.original.id.toUpperCase()}</span>
+      ),
+    }),
+    columnHelper.display({
+      id: "name",
+      header: () => t("products.table.title"),
+      cell: ({ row }) => (
+        <div className="flex flex-col">
+          <span>{row.original.name[locale]}</span>
+          <span className="text-xs text-muted-foreground">
+            {row.original.name[locale]}
+          </span>
+        </div>
+      ),
+    }),
+    columnHelper.accessor("createdAt", {
+      header: createSortableHeader(t("products.table.date")),
+      cell: ({ row }) => formatDate(row.original.createdAt, locale),
+    }),
+    columnHelper.accessor((product) => product.basePrice, {
+      id: "price",
+      header: createSortableHeader(t("products.table.price")),
+      cell: ({ row }) => (
+        <span className="font-medium">
+          {formatPrice(row.original.basePrice, locale)}
+        </span>
+      ),
+    }),
+  ];
+};
