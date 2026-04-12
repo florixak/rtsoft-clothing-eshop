d071e084f391a05435c07158abd9b2a3e8b97026
ptakondrej <florixakcz@seznam.cz>
2026-04-12 13:28:11 +0200

    feat: add products schema

--- src/lib/schema.ts
+++ src/lib/schema.ts
@@ -12,4 +12,14 @@ export const ordersSchema = z.object({
   perPage: z.coerce.number().int().positive().default(5).catch(5),
 });
 
+export const productsSchema = z.object({
+  productQ: z.string().optional(),
+  sort: z.enum(["createdAt", "amount"]).optional(),
+  sortOrder: z.enum(["asc", "desc"]).optional(),
+  status: z.enum(orderStatuses).optional(),
+  page: z.coerce.number().int().positive().default(1).catch(1),
+  perPage: z.coerce.number().int().positive().default(5).catch(5),
+});
+
 export type OrdersSearch = z.infer<typeof ordersSchema>;
+export type ProductsSearch = z.infer<typeof productsSchema>;
