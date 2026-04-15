df78bb145f6a2b24978dd6b0ab7e86a660d097b0
ptakondrej <florixakcz@seznam.cz>
2026-04-15 20:58:48 +0200

    feat: prefetch order in loader

--- src/routes/{-$locale}/admin/orders/$orderId.tsx
+++ src/routes/{-$locale}/admin/orders/$orderId.tsx
@@ -1,8 +1,18 @@
 import OrderDetails from "@/components/order/order-details";
+import { Skeleton } from "@/components/ui/skeleton";
+import { createOrderDetailsQueryOptions } from "@/hooks/query-options";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/{-$locale}/admin/orders/$orderId")({
   component: RouteComponent,
+  pendingComponent: () => <Skeleton className="h-10 w-full rounded" />,
+  errorComponent: () => <div>Error loading order details</div>,
+  loader: async ({ params, context }) => {
+    const { orderId } = params;
+    await context.queryClient.ensureQueryData(
+      createOrderDetailsQueryOptions(orderId),
+    );
+  },
 });
 
 function RouteComponent() {
