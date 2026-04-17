2d84cbe1836f00bb740ab3f3c72c540fa8fbce97
ptakondrej <florixakcz@seznam.cz>
2026-04-17 18:04:52 +0200

    feat: add user sidebar

---
+++ src/components/user/user-sidebar.tsx
@@ -0,0 +1,47 @@
+import { TRANSLATION_NAMESPACES } from "@/lib/i18n";
+import { useTranslation } from "react-i18next";
+import { Card, CardContent, CardHeader } from "../ui/card";
+import { Button } from "../ui/button";
+import { Link } from "@tanstack/react-router";
+import { USER_MENU_ITEMS } from "@/constants";
+
+const UserSidebar = () => {
+  const { t } = useTranslation(TRANSLATION_NAMESPACES.admin);
+  return (
+    <aside className="w-full md:w-64">
+      <Card className="w-full md:gap-6">
+        <CardHeader>
+          <h2 className="text-2xl font-bold">Dashboard</h2>
+        </CardHeader>
+        <CardContent>
+          <nav className="flex flex-row md:flex-col gap-2 w-full justify-center md:justify-start">
+            {USER_MENU_ITEMS.map((item) => (
+              <Button
+                nativeButton={false}
+                key={item.path}
+                className="w-full p-0"
+                variant="ghost"
+                render={() => (
+                  <Link
+                    to={item.path}
+                    className="flex flex-col md:flex-row items-center px-3 py-2 rounded-md hover:bg-muted gap-2"
+                    activeProps={{ className: "bg-muted" }}
+                    activeOptions={{ exact: true }}
+                  >
+                    <item.icon
+                      className="inline-block text-primary"
+                      size={20}
+                    />
+                    {t(item.label)}
+                  </Link>
+                )}
+              />
+            ))}
+          </nav>
+        </CardContent>
+      </Card>
+    </aside>
+  );
+};
+
+export default UserSidebar;
