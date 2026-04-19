3b35e6009bb106aee5e668f8e452658ba0c7f15a
ptakondrej <florixakcz@seznam.cz>
2026-04-19 10:13:59 +0200

    feat: redirect authenticated users from login

--- src/routes/{-$locale}/login.tsx
+++ src/routes/{-$locale}/login.tsx
@@ -1,8 +1,15 @@
 import Login from "@/components/auth/login";
-import { createFileRoute } from "@tanstack/react-router";
+import { isAuthenticated } from "@/lib/auth";
+import { createFileRoute, redirect } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/{-$locale}/login")({
   component: RouteComponent,
+  beforeLoad: async (/*{ context }*/) => {
+    const isLoggedIn = isAuthenticated();
+    if (isLoggedIn) {
+      throw redirect({ to: "/{-$locale}/account" });
+    }
+  },
 });
 
 function RouteComponent() {
