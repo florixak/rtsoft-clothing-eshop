f3915725b25011a9b4d43c33f246e5a38e48d9ce
ptakondrej <florixakcz@seznam.cz>
2026-04-19 10:08:16 +0200

    feat: create login page section

---
+++ src/components/auth/login.tsx
@@ -0,0 +1,11 @@
+import { LoginForm } from "./login-form";
+
+const Login = () => {
+  return (
+    <section className="container grid items-center gap-6 pt-6 pb-8 md:py-10 mx-auto max-w-md">
+      <LoginForm />
+    </section>
+  );
+};
+
+export default Login;
