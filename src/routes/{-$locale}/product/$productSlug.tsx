1348f88b3213b3f9990f62b9afb0f8960844de06
ptakondrej <florixakcz@seznam.cz>
2026-04-27 20:52:53 +0200

    feat: show ProductNotFound for missing product/category

--- src/routes/{-$locale}/product/$productSlug.tsx
+++ src/routes/{-$locale}/product/$productSlug.tsx
@@ -1,20 +1,34 @@
 import Product from "@/components/product/product";
+import ProductNotFound from "@/components/product/product-not-found";
+import { Skeleton } from "@/components/ui/skeleton";
 import {
   createCategoryQueryOptions,
   createProductSlugQueryOptions,
 } from "@/hooks/query-options";
-import { createFileRoute } from "@tanstack/react-router";
+import { createFileRoute, notFound } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/{-$locale}/product/$productSlug")({
   component: RouteComponent,
+  notFoundComponent: () => <ProductNotFound />,
+  errorComponent: () => <ProductNotFound />,
+  pendingComponent: () => <Skeleton className="h-10 w-full rounded" />,
   loader: async ({ context, params }) => {
     const { productSlug } = params;
-    const { categoryId } = await context.queryClient.ensureQueryData(
+    const product = await context.queryClient.ensureQueryData(
       createProductSlugQueryOptions(productSlug),
     );
-    await context.queryClient.ensureQueryData(
-      createCategoryQueryOptions(categoryId),
+
+    if (!product) {
+      throw notFound();
+    }
+
+    const category = await context.queryClient.ensureQueryData(
+      createCategoryQueryOptions(product.categoryId),
     );
+
+    if (!category) {
+      throw notFound();
+    }
   },
 });
 
