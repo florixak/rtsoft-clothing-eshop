12bc11f8b26df3c0983a0dbbdc0252386680b0f0
ptakondrej <florixakcz@seznam.cz>
2026-04-29 20:53:02 +0200

    chore: add skeletons index barrel file

---
+++ src/components/skeletons/index.ts
@@ -0,0 +1,7 @@
+export { TableSkeleton } from "./table-skeleton";
+export { AdminDashboardSkeleton } from "./admin-dashboard-skeleton";
+export { AccountLayoutSkeleton } from "./account-layout-skeleton";
+export { OrderDetailSkeleton } from "./order-detail-skeleton";
+export { ProductDetailSkeleton } from "./product-detail-skeleton";
+export { OrderListSkeleton } from "./order-list-skeleton";
+export { CatalogSkeleton } from "./catalog-skeleton";
